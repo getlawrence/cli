b81ef09a7c5b038360507e3cd77f86d04600c19d
Motti Bechhofer <mybechhofer@gmail.com>
2025-08-25 12:34:35 +0300

    Fix Homebrew formula with correct URLs and SHA256 hashes
    - Update URLs to use specific version v0.1.0-beta.5
    - Add actual SHA256 hashes from release checksums
    - Fix install method to handle lawrence-{OS}-{ARCH} binary names
    - Support for macOS ARM64, macOS AMD64, and Linux AMD64

--- homebrew/lawrence.rb
+++ homebrew/lawrence.rb
@@ -5,21 +5,28 @@ class Lawrence < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/getlawrence/cli/releases/latest/download/lawrence_darwin_arm64.tar.gz"
-      sha256 "placeholder_sha256_for_arm64" # This will be updated by the release workflow
+      url "https://github.com/getlawrence/cli/releases/download/v0.1.0-beta.5/lawrence_v0.1.0-beta.5_darwin_arm64.tar.gz"
+      sha256 "424ebdea6b860e353788cd0e287938b9fd02c9df62fbc1e5d63eda7e653cc8f6"
     else
-      url "https://github.com/getlawrence/cli/releases/latest/download/lawrence_darwin_amd64.tar.gz"
-      sha256 "placeholder_sha256_for_amd64" # This will be updated by the release workflow
+      url "https://github.com/getlawrence/cli/releases/download/v0.1.0-beta.5/lawrence_v0.1.0-beta.5_darwin_amd64.tar.gz"
+      sha256 "f2e44b42b3c6e4484c184a4dcfd0ad9c7c8d27d7911e080bab76f116194ea907"
     end
   end
 
   on_linux do
-    url "https://github.com/getlawrence/cli/releases/latest/download/lawrence_linux_amd64.tar.gz"
-    sha256 "placeholder_sha256_for_linux" # This will be updated by the release workflow
+    url "https://github.com/getlawrence/cli/releases/download/v0.1.0-beta.5/lawrence_v0.1.0-beta.5_linux_amd64.tar.gz"
+    sha256 "a0c55761f1c490aaead2269944b741d861a5f71cce5bad3a129c6a6b19c517bd"
   end
 
   def install
-    bin.install "lawrence"
+    # The archive contains lawrence-{OS}-{ARCH} binary
+    if Hardware::CPU.arm?
+      bin.install "lawrence-darwin-arm64" => "lawrence"
+    elsif OS.mac?
+      bin.install "lawrence-darwin-amd64" => "lawrence"
+    else
+      bin.install "lawrence-linux-amd64" => "lawrence"
+    end
   end
 
   test do
